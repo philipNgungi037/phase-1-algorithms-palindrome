ca08c85b0da1723da1d00dc78eeab1a1be760905
PHILIP <philophilo037@gmail.com>
2022-09-06 13:42:52 +0300

    Done

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversedArray = []
+let fwdArray
+let rvsString
+
+function isPalindrome(fwdString) {
+  stringSplitter(fwdString);
+      return fwdString ===`${rvsString}`
 }
 
-/* 
+
+
+
+
+function stringSplitter(string){
+  fwdArray = string.split("")
+  reverseSplitWord(fwdArray)
+}
+
+function reverseSplitWord(array){
+  for(let i = array.length; i> 0; i--){
+    reversedArray = [...reversedArray, array.pop()]
+
+  }
+  arraytoString(reversedArray)
+}
+
+function arraytoString (array){
+  debugger;
+  rvsString = array.join('');
+  reversedArray=[]
+}
+
+
+/*
   Add your pseudocode here
 */
 
@@ -12,14 +43,11 @@ function isPalindrome(word) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
-
   console.log("");
-
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
-
-module.exports = isPalindrome;
+module.exports = isPalindrome;}
